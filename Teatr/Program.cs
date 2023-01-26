d02e598efd6b3e8dc5e50aedcd51fae0036a29e2
Rafet <rafetr@risk.az>
2023-01-26 21:14:10 +0400

    dll

---
+++ Teatr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Teatr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(s);
+            sb.Insert(0, ' ');
+            char[] chars = new char[sb.Length];
+            //ab cd ef
+            //ef cd ab 
+          
+            int count = 0, k = 0;
+            for (int i = sb.Length - 1; i >= 0; i--)
+            {
+                count++;
+
+                var c = sb[i];
+
+                if (sb[i]==' ')
+                {
+                    sb.CopyTo(i + 1, chars, k, count - 1);
+                    k += count;
+                    chars[k - 1] = ' ';
+                    count = 0;
+                    
+                }
+            }
+
+            Console.WriteLine(new String(chars));
+        }
+    }
+}
---
+++ Teatr/Services/SeansManager.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using Core.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teatr.Services
+{
+    public class SeansManager : ICrudService
+    {
+        public void Add(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
